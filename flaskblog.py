7c4a90412f3d838cd1eb38db6e1c25d2e376e15b
DTKx <63344441+DTKx@users.noreply.github.com>
2021-02-13 13:16:08 -0300

    Finished implementation of part 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,3 +1,4 @@
+# Part 1
 #To run:
 # Activate venv
 # CMD set FLASK_APP=flaskblog.py
@@ -6,17 +7,38 @@
 # Note: To avoid restarting server every time we modify, we can view in debug mode
 # CMD set FLASK_DEBUG=1
 # Note: To run directly and protecting for the script being run directly use if __name__ then all one has to do is run the script
-from flask import Flask
+
+# Part 2
+# Use html Templates
+# Use template inheritance to avoid repeat code
+# Use a bootstrap example for a quick framework https://getbootstrap.com/docs/5.0/getting-started/introduction/#quick-start
+# Add some buttons
+
+from flask import Flask,render_template,url_for
 app=Flask(__name__)
 
+posts=[
+    {
+        'author':'Debora Kohara',
+        'title':'Blog Post 1',
+        'content': 'First post content: Hey you made this :) !',
+        'date_posted':'April 20,2018'
+    },
+    {
+        'author':'Debora 2',
+        'title':'Blog Post 2',
+        'content': 'Second post content: Hey you made this :) !',
+        'date_posted':'April 22,2018'
+    }
+]
 @app.route("/")#ROOT PAGE OF OUR PAGE
 @app.route("/home")#home also would work
 def hello():
-    return "<h1>Home Page</h1>"#Header
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")#ROOT PAGE OF OUR PAGE
 def about():
-    return "<h1>About Page</h1>"#Header
+    return render_template('about.html',title='About')
 
 if __name__ == "__main__":#This is only true if we run the script directly
     app.run(debug=True)
