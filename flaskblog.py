a59edc0d4dec6592af2eecd5756a9d423df382c2
DTKx <63344441+DTKx@users.noreply.github.com>
2021-02-16 10:04:40 -0300

    Part 3 Completed Forms and user input

--- flaskblog.py
+++ flaskblog.py
@@ -14,8 +14,14 @@
 # Use a bootstrap example for a quick framework https://getbootstrap.com/docs/5.0/getting-started/introduction/#quick-start
 # Add some buttons
 
-from flask import Flask,render_template,url_for
+# Part 3
+# Forms and User Input
+# Create a registration page, leverage from extensions that makes all 
+
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
+app.config['SECRET_KEY']='4ebebae9544f86c19558bc018c7ea517'# Secret Key protect form modified cookies,forgery attacks etc
 
 posts=[
     {
@@ -33,12 +39,32 @@ posts=[
 ]
 @app.route("/")#ROOT PAGE OF OUR PAGE
 @app.route("/home")#home also would work
-def hello():
+def home():
     return render_template('home.html',posts=posts)
 
 @app.route("/about")#ROOT PAGE OF OUR PAGE
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])#ROOT PAGE OF OUR PAGE
+# @app.route("/register")#ROOT PAGE OF OUR PAGE
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login",methods=['GET','POST'])#ROOT PAGE OF OUR PAGE
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=='admin@blog.com' and form.password.data=='password':
+            flash('You have been logged in','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password','danger')
+    return render_template('login.html',title='Login',form=form)
 
 if __name__ == "__main__":#This is only true if we run the script directly
     app.run(debug=True)
