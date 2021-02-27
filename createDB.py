2908e71eac8b42eecd2c9e18fd18e76699c0a98f
DTKx <63344441+DTKx@users.noreply.github.com>
2021-02-27 15:33:05 -0300

    Part 4 complete

---
+++ createDB.py
@@ -0,0 +1,35 @@
+#Creates db file 
+# It was runned in command line
+from flaskblog import db,User,Post
+
+db.create_all()
+
+user_1=User(username='Corey',email='c@gmail.com',password='password')
+db.session.add(user_1)
+user_2=User(username='Debs',email='debs@gmail.com',password='password')
+db.session.add(user_2)
+db.session.commit() #Commit to the database
+
+#Example of query to check
+print(User.query.all())
+print(User.query.first())
+print(User.query.filter_by(username='Corey').all())
+print(User.query.filter_by(username='Corey').first())
+user_c=User.query.filter_by(username='Corey').first()
+print(user_c)
+print(user_c.id)
+print(user_c.query.get(1))
+print(user_c.posts)
+
+post_1=Post(title='Blog1',content='First Post Content',user_id=user_c.id)
+post_2=Post(title='Blog2',content='First Post Content',user_id=user_c.id)
+db.session.add(post_1)
+db.session.add(post_2)
+db.session.commit()
+print(user_c.posts)
+for post in user_c.posts:
+    print(user_c.title)
+post_c=Post.query.first()
+print(post_c.user_id)
+print(post_c.author)#Works given backref
+# db.drop_all()#Drops all data so far
--- flaskblog.py
+++ flaskblog.py
@@ -18,10 +18,41 @@
 # Forms and User Input
 # Create a registration page, leverage from extensions that makes all 
 
+# Part 4
+# Client database
 from flask import Flask,render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
+from datetime import datetime
+
 app=Flask(__name__)
 app.config['SECRET_KEY']='4ebebae9544f86c19558bc018c7ea517'# Secret Key protect form modified cookies,forgery attacks etc
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'# Location of our sqllite, /// represents a relative path this means that our file should be created in the module we are currently in right now.
+db=SQLAlchemy(app)
+
+# Creates user models
+class User(db.Model):
+    # Creates Columns of our database
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(20),unique=True,nullable=False)#nullable, means we need to have one
+    email=db.Column(db.String(120),unique=True,nullable=False)#nullable, means we need to have one
+    image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
+    password=db.Column(db.String(60),nullable=False)#It will be hashable
+    posts=db.relationship('Post',backref='author',lazy=True)#One to many, backref allows retrieve all authors, lazy allow to query. Under the hood this is actually creating a sql query. Post is upper case becauese we are referencing the class
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    content=db.Column(db.Text,nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)#nullable each post have an author, user is lower case because we are referencing a table name that is automatically lowered
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
+
 
 posts=[
     {
